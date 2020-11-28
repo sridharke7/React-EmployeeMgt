48b03a4a343c26d462916ac55d658b13ca695258
sridharke7 <sridhar.kesavan@outlook.com>
2020-11-28 10:52:19 +0900

    viewalignment

--- src/components/ViewEmployeeComponent.jsx
+++ src/components/ViewEmployeeComponent.jsx
@@ -28,15 +28,15 @@ backToEmployee(){
                     <h3 className="text-center">View Employee</h3>
                         <div className="card-body">
                         <div className = "row">
-                            <label>First Name:</label>
+                            <label>First Name:  </label>
                             <div> {this.state.employee.firstName} </div>
                         </div>
                         <div className = "row">
-                            <label>Last Name:</label>
+                            <label>Last Name:   </label>
                             <div> {this.state.employee.lastName} </div>
                         </div>
                         <div className = "row">
-                            <label>Email ID:</label>
+                            <label>Email ID:    </label>
                             <div> {this.state.employee.emailId} </div>
                         </div>
                     </div>
